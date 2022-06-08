22939641499b2d7cbaa9d6e74e87cfaa48d2f265
HugoHernandez06 <44584564+HugoHernandez06@users.noreply.github.com>
2022-06-08 11:52:57 -0500

    Scroll - Sin responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,3 +210,26 @@ var swiper = new Swiper(".slide-content", {
         },
     },
   });
+
+  const registerVideo = (bound, video) => {
+	bound = document.querySelector(bound);
+	video = document.querySelector(video);
+	const scrollVideo = ()=>{
+		if(video.duration) {
+			const distanceFromTop = window.scrollY + bound.getBoundingClientRect().top;
+			const rawPercentScrolled = (window.scrollY - distanceFromTop) / (bound.scrollHeight - window.innerHeight);
+			const percentScrolled = Math.min(Math.max(rawPercentScrolled, 0), 1);
+			
+			video.currentTime = video.duration * percentScrolled;
+		}
+		requestAnimationFrame(scrollVideo);
+	}
+	requestAnimationFrame(scrollVideo);
+}
+
+
+registerVideo("#bound-one", "#bound-one video");
+registerVideo("#bound-two", "#bound-two video");
+registerVideo("#bound-three", "#bound-three video");
+registerVideo("#bound-four", "#bound-four video");
+registerVideo("#bound-five", "#bound-five video");
